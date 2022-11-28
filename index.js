ace213e6781cc5d323aed8801bf236996fd39c25
alexuschristensen <alexusdc6@gmail.com>
2022-11-27 16:39:50 -0800

    index.js file

--- index.js
+++ index.js
@@ -42,6 +42,58 @@ var cityLatLon = function() {
       return response.json();
     })
 
+    .then(function (data) {
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      var officialCity = data.name;
+      var nowIcon = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
+      userIcon.setAttribute("src", nowIcon);
+      var cityTitle = officialCity + " " + today;
+      var secondRequest = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&units=imperial&APPID=cae8444643fe0b52a066739e6b318cbd';
+      if(!localStorage.getItem("pastCity")){
+          var pastSearchBtn = document.createElement("button");
+          pastSearchBtn.textContent = officialCity;
+          pastSearches.append(pastSearchBtn);
+          pastSearchBtn.classList.add("btn-secondary", "btn-lg", "btn-block");
+          pastCity.push(pastSearchBtn.textContent.valueOf());
+          localStorage.setItem("pastCity",pastCity);
+      }else{
+          var pastCitiesArray = localStorage.getItem("pastCity").split(",");
+          if(!pastCitiesArray.includes(officialCity)){
+              var pastSearchBtn = document.createElement("button");
+              pastSearchBtn.textContent = officialCity;
+              pastSearches.append(pastSearchBtn);
+              pastSearchBtn.classList.add("btn-secondary", "btn-lg", "btn-block");
+              pastCity.push(pastSearchBtn.textContent.valueOf());
+              localStorage.setItem("pastCity",pastCity);
+          }
+      }   
+      
+      fetch(secondRequest)
+          .then(function (response) {
+              return response.json();
+          })
+          .then(function (data) {
+              userCity.textContent = cityTitle;
+              userTemp.textContent = "Temp: " + data.current.temp + "ºF";
+              userWind.textContent = "Wind: " + data.current.wind_speed + " MPH";
+              userHumidity.textContent = "Humidity: " + data.current.humidity + "%";
+              userUv.textContent = "UV Index: " + data.current.uvi;
+              for (let i = 0; i < h5El.length; i++) {
+                  var cardDates = data.daily[i+1].dt;
+                  var differentDates = moment(cardDates, "X").format("L");
+                  h5El[i].textContent = differentDates;
+                  var iconCard = 'http://openweathermap.org/img/wn/' + data.daily[i+1].weather[0].icon + '@2x.png';
+                  iconEl[i].setAttribute("src", iconCard);
+                  var humidCard = data.daily[i+1].humidity;
+                  humidityEl[i].textContent = "Humidity: " + humidCard + "%";
+                  var tempCard = data.daily[i+1].temp.max;
+                  tempEl[i].textContent = "Temp: " + tempCard + "ºF";
+                  var windCard = data.daily[i+1].wind_speed;
+                  windEl[i].textContent = "Wind: " + windCard + " MPH";
+              }
+          });
+  });
     
 };
 
@@ -67,3 +119,20 @@ var getCityWeather = function(repo) {
       }
     });
   };
+
+  if(localStorage.getItem("pastCity")){
+    var pastStoredCity = localStorage.getItem("pastCity");
+    pastStoredCity = pastStoredCity.split(",");
+    for (i=0; i < pastStoredCity.length; i++){
+        var pastStoredCityBtn = document.createElement("button");
+        pastSearches.append(pastStoredCityBtn);
+        pastStoredCityBtn.textContent = pastStoredCity[i];
+        pastStoredCityBtn.classList.add("btn-secondary", "btn-lg", "btn-block");
+    }
+}
+
+pastSearches.addEventListener("click", function(event){
+    event.preventDefault();
+    cityName=event.target.innerHTML;
+    getCoordinates();
+})
